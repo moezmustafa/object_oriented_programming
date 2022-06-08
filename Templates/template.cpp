4a41754c5c2e8cdced4adea561f2d4b0b2d108f6
Thinkpad <Thinkpad@moeez.com>
2022-06-08 05:12:34 +0100

    adsfads

--- Templates/template.cpp
+++ Templates/template.cpp
@@ -1,3 +1,23 @@
 #include<iostream>
 #include<string>
 
+//create a template 
+
+template<typename moeez>
+    void printarray(moeez *arr, int size)
+    {
+        for(int i=0; i<size; i++)
+        {
+            std::cout<<arr[i]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+    printarray(arr, 5);
+    std::string arr2[] = {"hello", "world"};
+    printarray(arr2, 2);
+    return 0;
+}
